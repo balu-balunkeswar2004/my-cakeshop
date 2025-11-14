3f3fb0dd77705c08421e7a222f0b497dcfacc05a
balu-balunkeswar2004 <balunkeswar.dey123@gmail.com>
2025-11-14 19:49:42 +0530

    first commit

--- src/components/maincontainer/MainContainer.jsx
+++ src/components/maincontainer/MainContainer.jsx
@@ -5,11 +5,21 @@ const MainContainer = () => {
     <div id='main_container'>/
       <div id='container'>
         <div id='home'>
-            <div>
-
-            </div>
         </div>
-        
+        <div id='location'>
+          <div id='address'>
+            <h1>Address:</h1>
+            <h1>Jagan Cake Bakery</h1>
+            <h2>Balitutha,Kujang,</h2>
+            <h2>Jagatsinghpur,Odisha,754141</h2>
+          </div>
+          <div id='price'>
+            <h1>Price:</h1>
+            <h2>1/2 kg:200 Rupees</h2>
+            <h2>1 kg:300 Rupees</h2>
+            <h2>Multi layer:1000 Rupees and above</h2>
+          </div>
+        </div>
       </div>
     </div>
   )
